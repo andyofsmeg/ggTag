6326af707d7ce66345d318a937df8ab4738ec1b1
Aimee Gott <agott@Mango.local>
2017-05-29 16:04:39 +0100

    #5 add test for addDateTime

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,17 @@
 context("Check that utilities behave as expected")
 
+test_that("Date correctly returned", {
+  
+  now <- Sys.time()
+  
+  dt <- addDateTime("%d%b%Y")
+  
+  now <- toupper(format(now, "%d%b%Y"))
+  
+  expect_equal(dt$label, now)
+  
+})
+
 test_that("Can count lines of meta information for a single meta item", {
   expect_equal(countMeta("abc\ndef"), 2)
   expect_equal(countMeta("abc\ndef\n"), 3)
