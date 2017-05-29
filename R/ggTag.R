64f8167739e5db03bb03624ff189863a7a5563a6
Aimee Gott <agott@mango-solutions.com>
2017-05-29 16:49:38 +0100

    #5 remove time related elements from main function

--- R/ggTag.R
+++ R/ggTag.R
@@ -59,17 +59,19 @@ ggTag <- function(object, raster = FALSE, useGGTitle = TRUE,
     object <- deleteGGTitle(object)
   }
   else theTitle <- title
-  
-  # Title and meta lines count
-  titleLines <- countMeta(theTitle)
-  metaLines <- countMetaMulti(meta, metaRight)
-  totalLinesTop <- metaLines + titleLines + 1.5
+  # Count title lines
+  titleLines <- length(unlist(str_split(theTitle, "\\n")))
+  metaLeftLines <- length(unlist(str_split(meta, "\\n")))
+  metaRightLines <- length(unlist(str_split(metaRight, "\\n")))
+  metaLines <- max(metaLeftLines, metaRightLines)
+
+  # Title and meta lines
+  # TODO: break into separate script and write tests
+  totalLinesTop <- titleLines + metaLines + 1.5
 
 	userID <- ifelse(username, Sys.getenv("USERNAME"), "")
 	projectPath <- ifelse(is.logical(path) && path, getwd(), path)
 	idAndProjectPath <- paste(userID, projectPath, sep = ": ")
-	theTime <- Sys.time()
-	theTime <- toupper(format(theTime, dateFormat))
 
 	# Plot
 	grid.newpage()
@@ -104,6 +106,7 @@ ggTag <- function(object, raster = FALSE, useGGTitle = TRUE,
 	}
 	popViewport()
 }
+
 
 
 
