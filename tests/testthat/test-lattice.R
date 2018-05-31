6ddf5b39a44872dee589c184353226ed9bd65d3d
Andy Nicholls <apn11864@wmservice.corpnet1.com>
2018-05-31 14:27:00 +0100

    A new test file to ensure lattice compatability (actually added in previous commit). Added title.

--- tests/testthat/test-lattice.R
+++ tests/testthat/test-lattice.R
@@ -2,7 +2,7 @@ context("Handles lattice objects")
 
 library(lattice)
 
-testGraph <- xyplot(Wind~Day, data = airquality)
+testGraph <- xyplot(Wind~Day, data = airquality, main = "Test")
 
 test_that("Defaults work with lattice objects", {
   expect_error(structure(ggTag(testGraph)), NA)
